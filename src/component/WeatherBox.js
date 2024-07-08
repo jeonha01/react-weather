5ae9a4261768096eb14457256cacdad4ab88acc9
Jeonha Kang <kangjeonha@gmail.com>
2024-07-08 20:25:15 +0900

    first commit

---
+++ src/component/WeatherBox.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const WeatherBox = ({ weather }) => {
+    console.log(weather)
+
+    return (
+        <div className='weather-box'>
+            <div>{weather?.name}</div>
+            <h2>{Math.ceil(weather?.main.temp)}도 / {Math.ceil(1.8 * (weather?.main.temp) + 32)}화씨</h2>
+            <h3>{weather?.weather[0].description}</h3>
+        </div>
+    )
+}
+
+export default WeatherBox
---
+++ src/component/WeatherButton.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Button } from 'react-bootstrap';
+
+const WeatherButton = ({ cities, variants }) => {
+    console.log(cities)
+    console.log(variants)
+
+    return (
+        <div>
+            <Button variant="secondary">Current Location</Button>
+            {/* <Button variant="primary">Paris</Button>
+            <Button variant="danger">München</Button>
+            <Button variant="light">London</Button>
+            <Button variant="warning">Madrid</Button> */}
+            {cities.map((item, index) => {
+                return (<Button variant={variants[index]}>{item}</Button>)
+            })}
+        </div>
+    )
+}
+
+export default WeatherButton
